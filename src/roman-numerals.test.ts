527e9496bd4be5c6e04e3f5f7e225f1008e8e2c6
Paquet--Kremer Robin <robin.paquet-kremer3@etu.univ-lorraine.fr>
2025-05-23 11:09:28 +0200

    Test

--- src/roman-numerals.test.ts
+++ src/roman-numerals.test.ts
@@ -1,1 +1,14 @@
+import { it, expect, test } from "vitest";
+
+class RomanNumeral {
+  of(value: string): number {
+    
+  }
+}
+
+let Convertiseur = new RomanNumeral()
+
 // Write your test here
+it("First 3I should give 1-2-3", function() {
+    expect(Convertiseur.of("I")).toBe(1)
+})
