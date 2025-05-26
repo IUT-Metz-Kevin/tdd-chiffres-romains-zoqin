0101a39d53aec539b4a48b9f1380ffe13ef1af48
Paquet--Kremer Robin <robin.paquet-kremer3@etu.univ-lorraine.fr>
2025-05-26 16:23:35 +0200

    REFACTORING

--- src/roman-numerals.test.ts
+++ src/roman-numerals.test.ts
@@ -38,41 +38,39 @@ class RomanNumeral {
 
     to(number: number): string {
         let romanString = "";
-        while((number-1000)>=0){
+        while ((number - 1000) >= 0) {
             romanString += "M"
-            number -=1000
+            number -= 1000
         }
-        while((number-500)>=0){
+        while ((number - 500) >= 0) {
             romanString += "D"
-            number -=500
+            number -= 500
         }
-        while((number-100)>=0){
+        while ((number - 100) >= 0) {
             romanString += "C"
-            number -=100
+            number -= 100
         }
-        while((number-50)>=0){
+        while ((number - 50) >= 0) {
             romanString += "L"
-            number -=50
+            number -= 50
         }
-        while((number-10)>=0){
+        while ((number - 10) >= 0) {
             romanString += "X"
-            number -=10
+            number -= 10
         }
-        while((number-5)>=0){
+        while ((number - 5) >= 0) {
             romanString += "V"
-            number -=5
+            number -= 5
         }
-        if(number===4) {
+        if (number === 4) {
             romanString += "IV";
             return romanString
         }
-        if(number<4) {
-            while(number>0){
-                romanString += "I";
-                number--
-            }
-            return romanString
-        }       
+        while (number > 0) {
+            romanString += "I";
+            number--
+        }
+        return romanString
     }
 }
 
@@ -142,44 +140,44 @@ it("should throw error for roman number in lower case", function () {
 });
 
 //to() test
-it("should give I-II-III for 1-3", function() {
+it("should give I-II-III for 1-3", function () {
     expect(convertiseurRoman.to(1)).toBe('I');
     expect(convertiseurRoman.to(2)).toBe('II');
     expect(convertiseurRoman.to(3)).toBe('III');
 })
 
-it("should give IV for 4", function() {
+it("should give IV for 4", function () {
     expect(convertiseurRoman.to(4)).toBe('IV');
 })
 
-it("should give V for 5", function() {
+it("should give V for 5", function () {
     expect(convertiseurRoman.to(5)).toBe('V');
 })
 
-it("should give X for 10", function() {
+it("should give X for 10", function () {
     expect(convertiseurRoman.to(10)).toBe('X');
 })
 
-it("should give L for 50", function() {
+it("should give L for 50", function () {
     expect(convertiseurRoman.to(50)).toBe('L');
 })
 
-it("should give C-CC-CCC for 100-200-300", function() {
+it("should give C-CC-CCC for 100-200-300", function () {
     expect(convertiseurRoman.to(100)).toBe('C');
     expect(convertiseurRoman.to(200)).toBe('CC');
     expect(convertiseurRoman.to(300)).toBe('CCC');
 })
 
-it("should give D for 500", function() {
+it("should give D for 500", function () {
     expect(convertiseurRoman.to(500)).toBe('D');
 })
 
-it("should give M-MM-MMM for 1000-2000-3000", function() {
+it("should give M-MM-MMM for 1000-2000-3000", function () {
     expect(convertiseurRoman.to(1000)).toBe('M');
     expect(convertiseurRoman.to(2000)).toBe('MM');
     expect(convertiseurRoman.to(3000)).toBe('MMM');
 })
 
-it("should give CD for 400", function() {
+it("should give CD for 400", function () {
     expect(convertiseurRoman.to(400)).toBe('CD');
 })
